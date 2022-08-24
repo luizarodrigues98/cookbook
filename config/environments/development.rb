3b6f4185a04ca3bacd5703e56cc65837e5736f99
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2022-08-24 17:38:33 -0300

    Adicionado devise e models admin e user

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,15 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  # Devise config
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # MailHog
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    address: 'localhost',
+    port: 1025
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :users_backoffice do
+    get 'welcome/index'
+  end
+  namespace :admins_backoffice do
+    get 'welcome/index'
+  end
+  devise_for :users
+  devise_for :admins
   root to: 'home#index'
   
   resources :recipes, except: :destroy do
