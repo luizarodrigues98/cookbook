b82b943f1fba8594c52c842f088131f3d3f44346
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2022-12-13 17:17:05 -0300

    adiciona o rspec, modifica views e adiciona o primeiro teste

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+
+  def nav_link_class(controller)
+    "nav-link px-2 link-#{controller_name == controller ? 'primary' : 'gray'}"
+  end
 end
--- test/controllers/admins_dashboard/admins_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class AdminsDashboard::AdminsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get admins_dashboard_admins_index_url
-    assert_response :success
-  end
-
-end
